7f5170a001a11ce1977587c327b1989f7903f234
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-06-03 17:00:22 +0600

    add card hover effects and display modal on card click

--- src/routes/(main)/punkWork/Card.tsx
+++ src/routes/(main)/punkWork/Card.tsx
@@ -1,5 +1,7 @@
 import { motion } from "motion/react";
-import { Play } from "lucide-react";
+import { useState } from "react";
+import { Play, Pause } from "lucide-react";
+import { Modal } from "@mantine/core";
 
 type CardData = {
   id: number;
@@ -12,27 +14,58 @@ type CardType = {
 };
 
 const Card = ({ data }: CardType) => {
+  const [opened, setOpened] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ amount: 0.2 }}
-      whileHover={{ y: -10 }}
-      transition={{ duration: 0.4, ease: "easeOut" }}
-      className="rounded-xl border border-[#C3C3C3] bg-[#F5F5F5] p-4"
-    >
-      <div className="mb-4 flex h-8 w-8 items-center justify-center rounded-full bg-[#DAD7D2]">
-        <Play size={14} fill="currentColor" />
-      </div>
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ amount: 0.2 }}
+        transition={{ duration: 0.4, ease: "easeOut" }}
+        onHoverStart={() => setHovered(true)}
+        onHoverEnd={() => setHovered(false)}
+        className="relative h-95 overflow-hidden rounded-xl border border-[#C3C3C3] bg-[#F5F5F5] p-4"
+      >
+        {hovered ? (
+          <Pause size={14} fill="currentColor" />
+        ) : (
+          <Play size={14} fill="currentColor" />
+        )}
 
-      <h3 className="mb-2 text-xl font-semibold text-(--primary-color)">
-        {data.title}
-      </h3>
+        <h3 className="mb-2 text-xl font-semibold text-(--primary-color)">
+          {data.title}
+        </h3>
 
-      <p className="text-sm leading-relaxed text-(--secondary-color)">
-        {data.description}
-      </p>
-    </motion.div>
+        <p className="text-sm leading-relaxed text-(--secondary-color)">
+          {data.description}
+        </p>
+
+        <motion.button
+          onClick={() => setOpened(true)}
+          animate={{
+            y: hovered ? 10 : 80,
+          }}
+          transition={{
+            duration: 0.35,
+            ease: "easeInOut",
+          }}
+          className="absolute bottom-0 left-1/2 h-28 w-[80%] -translate-x-1/2 rounded-t-xl bg-white shadow-[0_-4px_20px_rgba(0,0,0,0.08)] cursor-pointer"
+        ></motion.button>
+      </motion.div>
+
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        centered
+        styles={{
+          body: {
+            height: "400px",
+          },
+        }}
+      ></Modal>
+    </>
   );
 };
 
