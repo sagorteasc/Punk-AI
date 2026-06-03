d47997a2446cec7d1fc6a361d902111fd48b4249
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-06-03 18:45:36 +0600

    update input field styling

--- src/routes/(main)/sellPrompt/YoutubeVideo.tsx
+++ src/routes/(main)/sellPrompt/YoutubeVideo.tsx
@@ -28,11 +28,26 @@ const YoutubeVideo = () => {
       </div>
 
       <div className="mx-auto max-w-xl">
-        <div className="flex overflow-hidden rounded-2xl bg-[#fafafa] drop-shadow-2xl shadow-2xl w-11/12 mx-auto">
+        <div
+          className="flex overflow-hidden rounded-2xl bg-[#fafafa] w-11/12 mx-auto"
+          style={{
+            boxShadow: `
+              0 6.31px 13.68px rgba(0,0,0,0.12),
+              0 24.2px 24.2px rgba(0,0,0,0.22),
+              0 53.65px 32.61px rgba(0,0,0,0.06),
+              0 95.73px 37.87px rgba(0,0,0,0.02),
+              0 150.44px 42.08px rgba(0,0,0,0),
+              inset 0 3.16px 10.52px rgba(255,255,255,0.17),
+              inset 1.05px 1.05px 1.05px rgba(255,255,255,0.4),
+              inset -1.05px -1.05px 2.1px rgba(255,255,255,0.25)
+            `,
+            background: "linear-gradient(180deg, #FAFAFA 0%, #E6E6E6 100%)",
+          }}
+        >
           <input
             type="email"
             placeholder="Enter Email Address"
-            className="flex-1 bg-transparent px-1.5 md:px-6 py-5 text-[#6B6B6B] outline-none"
+            className="flex-1 bg-transparent px-4 md:px-6 py-5 text-[#6B6B6B] outline-none"
           />
 
           <button className="bg-linear-to-bl from-[#313131] to-[#222222] px-2 md:px-5 text-[8px]! md:text-sm! font-bold text-white rounded-lg m-1.5 hover:cursor-pointer">
